4b72105f53fa34274c3b9daf127bed6451d091d2
francescobraicovich <francesco.braicovich@gmail.com>
2024-11-28 02:01:48 +0100

    Refactor ARC environment to integrate Gymnasium, update state extraction, and enhance reward calculations

--- code/enviroment.py
+++ code/enviroment.py
@@ -1,42 +1,50 @@
 import numpy as np
 import random
 from dsl.utilities.padding import pad_grid, unpad_grid
+import gymnasium as gym
+from gymnasium import spaces
 
-def extract_states(training_grid0, training_grid1, dim):
+def extract_states(previous_state, current_state,  target_state):
     """
     Extract the current and target states from the training grids.
     """
-    current_state0 = training_grid0[:, :dim] # get the current state before the action
-    current_state1 = training_grid1[:, :dim] # get the current state after the action
-    target_state = training_grid0[:, dim:] # get the target state
-    
     # Remove padding
-    current_state0 = unpad_grid(current_state0)
-    current_state1 = unpad_grid(current_state1)
-    target_state = unpad_grid(target_state)
+    previous_state_not_padded = unpad_grid(previous_state)
+    current_state_not_padded = unpad_grid(current_state)
+    target_state_not_padded = unpad_grid(target_state)
 
-    return current_state0, current_state1, target_state
+    return previous_state_not_padded, current_state_not_padded, target_state_not_padded
 
-
-class ARCEnviroment():
+class ARC_Env(gym.Env):
     def __init__(self, challenge_dictionary, dim=30):
-        self.challenge_dictionary = challenge_dictionary
-        self.num_challenges = len(challenge_dictionary)
+        super(ARC_Env, self).__init__()
+        
+        self.challenge_dictionary = challenge_dictionary # dictionary of challenges
+        self.dictionary_keys = list(challenge_dictionary.keys()) # list of keys in the dictionary
+        self.num_challenges = len(challenge_dictionary) # number of challenges in the dictionary
         self.dim = dim # maximum dimension of the problem
+        self.observation_shape = (1, dim, 2*dim) # shape of the grid
 
         # reward variables
         self.step_penalty = -1
         self.maximum_similarity = 50
         self.completed_challenge_reward = 25
 
-        # Initialize current training grid
-        self.current_training_grid = self.get_random_challenge()
+        # Define the action space: a sequence of 9 integers
+        self.action_space = spaces.Box(low=0, high=255, shape=(9,), dtype=np.int32)
+        
+        # Define the observation space: a 60x30 image with pixel values between 0 and 255
+        self.observation_space = spaces.Box(low=0, high=255, shape=self.observation_shape, dtype=np.uint8)
+
+        # Define the state of the environment
+        self.state = np.zeros(self.observation_shape, dtype=np.uint8)
+        self.done = False
 
     def get_random_challenge(self):
         """
         Get a random challenge from the challenge dictionary. 
         """
-        challenge_key = random.choice(list(self.challenge_dictionary.keys())) 
+        challenge_key = random.choice(self.dictionary_keys) 
         challenge = self.challenge_dictionary[challenge_key]
         challenge_train = challenge['train']
         challenge_length = len(challenge_train)
@@ -44,25 +52,27 @@ class ARCEnviroment():
         random_challenge = challenge_train[random_index]
         random_input = np.array(random_challenge['input'])
         random_output = np.array(random_challenge['output'])
-        random_input = pad_grid(random_input)
-        random_output = pad_grid(random_output)
-        training_grid = np.hstack((random_input, random_output))
+        
+        # Pad the grids
+        training_grid = np.zeros(self.observation_shape, dtype=np.uint8)
+        training_grid[:, :, :self.dim] = pad_grid(random_input)
+        training_grid[:, :, self.dim:] = pad_grid(random_output)
         return training_grid
     
     def reset(self):
-        self.current_training_grid = self.get_random_challenge()
-        return self.current_training_grid
+        self.state = self.get_random_challenge()
+        self.done = False
+        return self.state
     
-    def shape_reward(self, current_state0, current_state1, target_state):
+    def shape_reward(self, previous_state_unpadded, current_state_unpadded, target_state):
         """
         Reward the agent based on the shape of the grid.
         Outputs:
         - reward: the reward for the agent based on the shape of the grid
         - shapes_agree: a boolean indicating if the shapes of the 3 grids agree
         """
-
-        current_shape0 = np.shape(current_state0)
-        current_shape1 = np.shape(current_state1)
+        current_shape0 = np.shape(previous_state_unpadded)
+        current_shape1 = np.shape(current_state_unpadded)
         target_shape = np.shape(target_state)
         
         # If the transformation does not change the shape of the grid
@@ -94,44 +104,63 @@ class ARCEnviroment():
             else:
                 return 5, False
           
-    def similarity_reward(self, current_state0, current_state1, target_state):
+    def similarity_reward(self, previous_state_unpadded, current_state_unpadded, target_state):
         """
         Reward the agent based on the similarity of the grid.
         """ 
         size = np.size(target_state)
         # If the shapes agree, we compute the similarity reward
-        similarity_0 = np.sum(current_state0 == target_state) / size
-        similarity_1 = np.sum(current_state1 == target_state) / size
+        similarity_0 = np.sum(previous_state_unpadded == target_state) / size
+        similarity_1 = np.sum(current_state_unpadded == target_state) / size
 
         similarity_difference = similarity_1 - similarity_0
         return self.maximum_similarity * similarity_difference
 
-    def total_reward(self, current_state0, current_state1, target_state):
+    def total_reward(self, previous_state_unpadded, current_state_unpadded, target_state):
         """
         Compute the total reward for the agent.
         """
 
-        shape_reward, shapes_agree = self.shape_reward(current_state0, current_state1, target_state)
+        shape_reward, shapes_agree = self.shape_reward(previous_state_unpadded, current_state_unpadded, target_state)
         if not shapes_agree: # If the shapes do not agree, we return the shape reward
             return shape_reward
         
         # If the shapes agree, we compute the similarity reward
-        similarity_reward = self.similarity_reward(current_state0, current_state1, target_state)
+        similarity_reward = self.similarity_reward(previous_state_unpadded, current_state_unpadded, target_state)
         
         # If the agent has completed the challenge, we return the completed challenge reward
-        if np.all(current_state1 == target_state):
-            return shape_reward + similarity_reward + self.completed_challenge_reward
+        if np.all(current_state_unpadded == target_state):
+            return shape_reward + similarity_reward + self.completed_challenge_reward, True
         
         # If the agent has not completed the challenge, we return the sum of the shape and similarity rewards
-        return shape_reward + similarity_reward + self.step_penalty
-
-    def step(self, action):
-        dim = self.dim        
-        current_state0 = self.current_training_grid[:, :dim]
-
-        # Apply the action to the current state
-        current_state1 = action(current_state0)
+        return shape_reward + similarity_reward + self.step_penalty, False
+    
+    def act(self, previous_state, action):
+        """
+        Apply the action to the previous state.
+        """
+        color_selection = action[0]
+        selection = action[1:3]
+        selection_parameters = action[3:5]
+        transformation = action[5:7]
+        transformation_parameters = action[7:]
 
         
 
-        pass
+    def step(self, action):
+        previous_state = self.state[:, :self.dim] # make the current state into the previous state
+        current_state = self.act(previous_state, action) # apply the action to the previous state
+        target_state = self.state[:, self.dim:] # get the target state
+
+        # Extract the states without padding
+        previous_state_not_padded, current_state_not_padded, target_state_not_padded = extract_states(previous_state, current_state, target_state)
+        reward, done = self.total_reward(previous_state_not_padded, current_state_not_padded, target_state_not_padded) # compute the reward
+        
+        # End the episode with a small probability if not ended already
+        if not done and random.random() < 0.01:
+            done = True
+    
+        # Update the state and done variables
+        self.state[:, :self.dim] = current_state
+        self.done = done
+        return self.state, reward, done, {}
