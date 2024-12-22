7b2c4c4013d38a0d5ab2e2404a68cab4b098a092
francescobraicovich <francesco.braicovich@gmail.com>
2024-12-22 21:13:39 +0100

    feat: enhance WolpertingerAgent to support variable k for nearest neighbors and improve action selection

--- src/wolp_agent.py
+++ src/wolp_agent.py
@@ -6,14 +6,13 @@ import torch
 criterion = nn.MSELoss()
 
 class WolpertingerAgent(DDPG):
-    def __init__(self, nb_states, nb_actions, args):
+    def __init__(self, nb_states, nb_actions, args, k=5):
         super().__init__(args, nb_states, nb_actions)
-        
-        print('Initializing Wolpertinger Agent')
         self.experiment = args.id
         
         # according to the papers, it can be scaled to hundreds of millions
         self.action_space = action_space.ARCActionSpace()
+        self.k_nearest_neighbors = k
 
     def get_name(self):
         return 'Wolp3_{}k{}_{}'.format(self.action_space.get_number_of_actions(),
@@ -24,86 +23,64 @@ class WolpertingerAgent(DDPG):
 
     def wolp_action(self, s_t, proto_action):
         # get the proto_action's k nearest neighbors
-        distances, indices, actions = self.action_space.search_point(proto_action, k=5)
-        print("distances", distances)
-        print("indices", indices)
-        print("actions", actions)
+        print('Getting wolp action')
+        print('s_t shape', s_t.shape)
+        distances, indices, actions = self.action_space.search_point(proto_action, k=self.k_nearest_neighbors)
+        print('Actions shape', actions.shape)
 
         if not isinstance(s_t, np.ndarray):
            s_t = to_numpy(s_t, gpu_used=self.gpu_used)
+
         # make all the state, action pairs for the critic
-        s_t = np.tile(s_t, [raw_actions.shape[1], 1])
-        print("s_t", s_t)
-        print('Shape of s_t:', s_t.shape)
+        s_t = np.tile(s_t, (self.k_nearest_neighbors, 1, 1))
+        print('Tiled state shape', s_t.shape)
 
-        s_t = s_t.reshape(len(raw_actions), raw_actions.shape[1], s_t.shape[1]) if self.k_nearest_neighbors > 1 \
-            else s_t.reshape(raw_actions.shape[0], s_t.shape[1])
-        print("s_t", s_t)
-        print('Shape of s_t:', s_t.shape)
-
-        raw_actions = to_tensor(raw_actions, gpu_used=self.gpu_used, gpu_0=self.gpu_ids[0])
-        print("raw_actions", raw_actions)
-        print('Shape of raw_actions:', raw_actions.shape)
+        actions = to_tensor(actions, gpu_used=self.gpu_used, gpu_0=self.gpu_ids[0])
         s_t = to_tensor(s_t, gpu_used=self.gpu_used, gpu_0=self.gpu_ids[0])
-        print("s_t", s_t)
-        print('Shape of s_t:', s_t.shape)
-
         # evaluate each pair through the critic
-        actions_evaluation = self.critic([s_t, raw_actions])
+        actions_evaluation = self.critic(s_t, actions)
 
         # find the index of the pair with the maximum value
-        max_index = np.argmax(to_numpy(actions_evaluation, gpu_used=self.gpu_used), axis=1)
-        max_index = max_index.reshape(len(max_index),)
-
-        raw_actions = to_numpy(raw_actions, gpu_used=self.gpu_used)
+        max_index = np.argmax(to_numpy(actions_evaluation, gpu_used=self.gpu_used).ravel())
+        actions = to_numpy(actions, gpu_used=self.gpu_used)
         # return the best action, i.e., wolpertinger action from the full wolpertinger policy
-        if self.k_nearest_neighbors > 1:
-            return raw_actions[[i for i in range(len(raw_actions))], max_index, [0]].reshape(len(raw_actions),1), \
-                   actions[[i for i in range(len(actions))], max_index, [0]].reshape(len(actions),1)
-        else:
-            return raw_actions[max_index], actions[max_index]
+        return actions[max_index]
 
-    def select_action(self, s_t, decay_epsilon=True):
+    def select_action(self, s_t, shape, decay_epsilon=True):
         # taking a continuous action from the actor
-        proto_action = super().select_action(s_t, decay_epsilon)
-
-        raw_wolp_action, wolp_action = self.wolp_action(s_t, proto_action)
-        assert isinstance(raw_wolp_action, np.ndarray)
-        self.a_t = raw_wolp_action
+        proto_action = super().select_action(s_t, shape,  decay_epsilon)
+        wolp_action = self.wolp_action(s_t, proto_action)
+        assert isinstance(wolp_action, np.ndarray)
+        self.a_t = wolp_action
         # return the best neighbor of the proto action, this is an action for env step
-        return wolp_action[0]  # [i]
+        return self.a_t
 
     def random_action(self):
         proto_action = super().random_action()
-        print("proto_action", proto_action)
         distances, indices, actions = self.action_space.search_point(proto_action, 1)
-        print("distances", distances)
-        print("indices", indices)
-        print("actions", actions)
         action = actions[0]
-        print("action", action)
-        if action not in self.action_space.space:
-            print('Action not in action space')
-            raise ValueError('Action not in action space')
-        assert isinstance(action, np.ndarray)
+        self.a_t = action
         return action
 
     def select_target_action(self, s_t):
         proto_action = self.actor_target(s_t)
         proto_action = to_numpy(torch.clamp(proto_action, -1.0, 1.0), gpu_used=self.gpu_used)
-
-        raw_wolp_action, wolp_action = self.wolp_action(s_t, proto_action)
-        return raw_wolp_action
+        action = self.wolp_action(s_t, proto_action)
+        return action
 
     def update_policy(self):
+        print('Updating policy')
         # Sample batch
         state_batch, action_batch, reward_batch, \
         next_state_batch, terminal_batch = self.memory.sample_and_split(self.batch_size)
+        print('Sampled batch')
 
         # Prepare for the target q batch
         # the operation below of critic_target does not require backward_P
-        next_state_batch = to_tensor(next_state_batch, volatile=True, gpu_used=self.gpu_used, gpu_0=self.gpu_ids[0])
+        next_state_batch = to_tensor(next_state_batch)
+        print('Prepared next state batch')
         next_wolp_action_batch = self.select_target_action(next_state_batch)
+        print('Prepared target q batch')
         # print(next_state_batch.shape)
         # print(next_wolp_action_batch.shape)
         next_q_values = self.critic_target([
@@ -118,6 +95,7 @@ class WolpertingerAgent(DDPG):
                          self.gamma * \
                          to_tensor(terminal_batch.astype(np.float64), gpu_used=self.gpu_used, gpu_0=self.gpu_ids[0]) * \
                          next_q_values
+        print('Prepared target q batch')
 
         # Critic update
         self.critic.zero_grad()  # Clears the gradients of all optimized torch.Tensor s.
@@ -125,6 +103,8 @@ class WolpertingerAgent(DDPG):
         state_batch = to_tensor(state_batch, gpu_used=self.gpu_used, gpu_0=self.gpu_ids[0])
         action_batch = to_tensor(action_batch, gpu_used=self.gpu_used, gpu_0=self.gpu_ids[0])
         q_batch = self.critic([state_batch, action_batch])
+
+        print('Prepared q batch')
 
         value_loss = criterion(q_batch, target_q_batch)
         value_loss.backward()  # computes gradients
