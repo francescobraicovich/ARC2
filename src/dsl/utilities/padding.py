7461f50a7b02e52053fd6da607003c605a0281a9
francescobraicovich <francesco.braicovich@gmail.com>
2024-12-22 21:13:42 +0100

    fix: simplify padding and unpadding functions to handle grid dimensions directly

--- src/dsl/utilities/padding.py
+++ src/dsl/utilities/padding.py
@@ -12,19 +12,18 @@ def pad_grid(grid: np.ndarray, desired_dim: tuple[int, int]=(30, 30)) -> np.ndar
     """
     desired_rows, desired_cols = desired_dim
     n_rows, n_cols = grid.shape
-    padded_grid = - np.ones((1, desired_rows, desired_cols), dtype=int)
-    padded_grid[0, :n_rows, :n_cols] = grid
+    padded_grid = - np.ones((desired_rows, desired_cols), dtype=int)
+    padded_grid[:n_rows, :n_cols] = grid
     return padded_grid
 
 def unpad_grid(grid: np.ndarray) -> np.ndarray:
     """
     Extract the original grid from the padded grid while preserving its original shape.
     """
-    grid_2d = grid[0]
-    mask = (grid_2d != -1)
+    mask = (grid != -1)
     rows, cols = np.where(mask)
     if rows.size == 0 and cols.size == 0:
         return np.zeros((0, 0), dtype=int)
     unpadded = np.zeros((max(rows)+1, max(cols)+1), dtype=int)
-    unpadded[rows, cols] = grid_2d[mask]
+    unpadded[rows, cols] = grid[mask]
     return unpadded
