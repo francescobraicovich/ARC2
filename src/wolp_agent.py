2abf62e6cd456ea23e1f17222d0a373e76e69831
francescobraicovich <francesco.braicovich@gmail.com>
2024-12-24 03:22:16 +0100

    refactor: enhance device management and tensor operations in WolpertingerAgent

--- src/wolp_agent.py
+++ src/wolp_agent.py
@@ -8,11 +8,27 @@ criterion = nn.MSELoss()
 class WolpertingerAgent(DDPG):
     def __init__(self, nb_states, nb_actions, args, k=5):
         super().__init__(args, nb_states, nb_actions)
+
+        # Automatically determine the device
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+            print("Using CUDA for WolpertingerAgent")
+        elif torch.backends.mps.is_available():
+            self.device = torch.device("mps")
+            print("Using MPS for WolpertingerAgent")
+        else:
+            self.device = torch.device("cpu")
+            print("Using CPU for WolpertingerAgent")
+
         self.experiment = args.id
-        
-        # according to the papers, it can be scaled to hundreds of millions
         self.action_space = action_space.ARCActionSpace()
         self.k_nearest_neighbors = k
+
+        # Move all networks to the determined device
+        self.actor.to(self.device)
+        self.actor_target.to(self.device)
+        self.critic.to(self.device)
+        self.critic_target.to(self.device)
 
     def get_name(self):
         return 'Wolp3_{}k{}_{}'.format(self.action_space.get_number_of_actions(),
@@ -22,111 +38,96 @@ class WolpertingerAgent(DDPG):
         return self.action_space
 
     def wolp_action(self, s_t, shape, proto_action):
-        # get the proto_action's k nearest neighbors
+        # Get the proto_action's k nearest neighbors
         distances, indices, actions = self.action_space.search_point(proto_action, k=self.k_nearest_neighbors)
+        actions = to_tensor(actions, device=self.device) #NOTE: next step is to move the action space to the device as tensors
 
+        # Make all the state, action pairs for the critic        
+        s_t = torch.tile(s_t, (self.k_nearest_neighbors, 1, 1, 1, 1))
+        shape = torch.tile(shape, (self.k_nearest_neighbors, 1, 1, 1))
 
-        if not isinstance(s_t, np.ndarray):
-           s_t = to_numpy(s_t, gpu_used=self.gpu_used)
+        actions = torch.reshape(actions, (self.k_nearest_neighbors, s_t.shape[1], self.nb_actions))
 
-        # make all the state, action pairs for the critic
-        s_t = np.tile(s_t, (self.k_nearest_neighbors, 1, 1, 1, 1))
-        shape = np.tile(shape, (self.k_nearest_neighbors, 1, 1, 1))
-
-        #print('Reshaping actions into: ', (self.k_nearest_neighbors,s_t.shape[1], self.nb_actions))
-        actions = np.reshape(actions, (self.k_nearest_neighbors,s_t.shape[1], self.nb_actions))
-        actions = to_tensor(actions)
-        s_t = to_tensor(s_t)
-        shape = to_tensor(shape)
-        #print('Tiled shape shape', shape.shape)
-        
+        #actions = to_tensor(actions, device=self.device)
         x = s_t, shape
-        
-        # evaluate each pair through the critic
+
+        # Evaluate each pair through the critic
         actions_evaluation = torch.squeeze(self.critic(x, actions))
 
+        # Find the index of the pair with the maximum value
+        max_index = torch.argmax(actions_evaluation, dim=0)
 
-        # find the index of the pair with the maximum value
-        max_index = np.argmax(to_numpy(actions_evaluation, gpu_used=self.gpu_used), axis=0)
- 
-        actions = to_numpy(actions, gpu_used=self.gpu_used)
-        selected_action = actions[max_index, np.arange(actions.shape[1]), :]
-        if selected_action.shape[0] == 1:
+        # Select the actions based on the maximum index
+        selected_action = actions[max_index, torch.arange(actions.size(1)), :]
+
+        # Adjust shape if necessary
+        if selected_action.size(0) == 1:
             selected_action = selected_action[0]
-        
-        #print('Selected action', selected_action)
-        # return the best action, i.e., wolpertinger action from the full wolpertinger policy
+        selected_action = to_numpy(selected_action, device=self.device)
+
         return selected_action
 
     def select_action(self, s_t, shape, decay_epsilon=True):
-        # taking a continuous action from the actor
-        proto_action = super().select_action(s_t, shape,  decay_epsilon)
+        # Take a continuous action from the actor
+        proto_action = super().select_action(s_t, shape, decay_epsilon)
+    
         wolp_action = self.wolp_action(s_t, shape, proto_action)
         assert isinstance(wolp_action, np.ndarray)
-        self.a_t = wolp_action
-        # return the best neighbor of the proto action, this is an action for env step
-        return self.a_t
+        self.a_t = to_tensor(wolp_action, device=self.device)
+        return wolp_action
 
     def random_action(self):
         proto_action = super().random_action()
         distances, indices, actions = self.action_space.search_point(proto_action, 1)
         action = actions[0]
-        self.a_t = action
+        self.a_t = to_tensor(action, device=self.device)
         return action
 
     def select_target_action(self, s_t, shape):
         x = s_t, shape
 
         proto_action = self.actor_target(x)
-        proto_action = to_numpy(torch.clamp(proto_action, -1.0, 1.0), gpu_used=self.gpu_used)
+        proto_action = to_numpy(torch.clamp(proto_action, -1.0, 1.0), device=self.device)
         action = self.wolp_action(s_t, shape, proto_action)
         return action
 
     def update_policy(self):
-        
         # Sample batch
         state_batch, shape_batch, action_batch, reward_batch, \
-        next_state_batch, next_shape_batch, terminal_batch = self.memory.sample_and_split(self.batch_size)
-
+            next_state_batch, next_shape_batch, terminal_batch = self.memory.sample_and_split(self.batch_size)
+        
         # Prepare for the target q batch
-        # the operation below of critic_target does not require backward_P
-        next_state_batch = to_tensor(next_state_batch)
-        next_shape_batch = to_tensor(next_shape_batch)
+        #next_state_batch = to_tensor(next_state_batch, device=self.device)
+        #next_shape_batch = to_tensor(next_shape_batch, device=self.device)
         next_wolp_action_batch = self.select_target_action(next_state_batch, next_shape_batch)
-        
-        next_states_expaneded = next_state_batch.unsqueeze(0)
-        next_shape_expaneded = next_shape_batch.unsqueeze(0)
-        next_action_batch_expaneded = to_tensor(next_wolp_action_batch).unsqueeze(0)
-        next_state = (next_states_expaneded, next_shape_expaneded)
-        
-        # next_q_values = Q'(s', wolp_action(s')): calculate the target q value
-        next_q_values = self.critic_target(next_state, next_action_batch_expaneded)
 
-        # but it requires bp in computing gradient of critic loss
-        next_q_values.volatile = False
+        next_states_expanded = next_state_batch.unsqueeze(0)
+        next_shape_expanded = next_shape_batch.unsqueeze(0)
+        next_action_batch_expanded = to_tensor(next_wolp_action_batch, device=self.device).unsqueeze(0)
+        next_state = (next_states_expanded, next_shape_expanded)
 
-        # next_q_values = 0 if is terminal states
-        target_q_batch = to_tensor(reward_batch) + self.gamma * to_tensor(terminal_batch) * next_q_values
+        # Next Q values
+        next_q_values = self.critic_target(next_state, next_action_batch_expanded)
+
+        # Handle terminal states
+        target_q_batch = reward_batch + self.gamma * terminal_batch * next_q_values
 
         # Critic update
-        self.critic.zero_grad()  # Clears the gradients of all optimized torch.Tensor s.
-        state_batch_unsqueezed = to_tensor(state_batch).unsqueeze(0)
-        shape_batch_unsqueezed = to_tensor(shape_batch).unsqueeze(0)
-        action_batch_unsqueezed = to_tensor(action_batch).unsqueeze(0)
- 
+        self.critic.zero_grad()
+        state_batch_unsqueezed = state_batch.unsqueeze(0)
+        shape_batch_unsqueezed = shape_batch.unsqueeze(0)
+        action_batch_unsqueezed = action_batch.unsqueeze(0)
+
         state_unsqueezed = (state_batch_unsqueezed, shape_batch_unsqueezed)
         q_batch = self.critic(state_unsqueezed, action_batch_unsqueezed)
 
         value_loss = criterion(q_batch, target_q_batch)
-        value_loss.backward()  # computes gradients
-        self.critic_optim.step()  # updates the parameters
+        value_loss.backward()
+        self.critic_optim.step()
 
         # Actor update
         self.actor.zero_grad()
-
-        # self.actor(to_tensor(state_batch)): proto_action_batch
-
-        proto_action_batch = self.actor((to_tensor(state_batch), to_tensor(shape_batch)))
+        proto_action_batch = self.actor((state_batch, shape_batch))
         proto_action_batch_unsqueezed = proto_action_batch.unsqueeze(0)
         policy_loss = -self.critic(state_unsqueezed, proto_action_batch_unsqueezed)
         policy_loss = policy_loss.mean()
