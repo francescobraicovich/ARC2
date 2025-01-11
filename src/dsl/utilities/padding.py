93fafd72bcc627759028f4980bfa2832f29129f9
francescobraicovich <francesco.braicovich@gmail.com>
2025-01-11 14:42:38 +0100

    fix: change dtype to np.int16 in padding functions for consistency

--- src/dsl/utilities/padding.py
+++ src/dsl/utilities/padding.py
@@ -6,13 +6,15 @@ import numpy as np
 # - pad_grid(grid, desired_dim): Pad the grid to the desired dimensions.
 # - remove_padding(grid): Extract the original grid from the padded grid.
 
+dtype = np.int16
+
 def pad_grid(grid: np.ndarray, desired_dim: tuple[int, int]=(30, 30)) -> np.ndarray:
     """
     Pad the grid to the desired dimensions.
     """
     desired_rows, desired_cols = desired_dim
     n_rows, n_cols = grid.shape
-    padded_grid = - np.ones((desired_rows, desired_cols), dtype=int)
+    padded_grid = - np.ones((desired_rows, desired_cols), dtype=dtype)
     padded_grid[:n_rows, :n_cols] = grid
     return padded_grid
 
@@ -24,6 +26,6 @@ def unpad_grid(grid: np.ndarray) -> np.ndarray:
     rows, cols = np.where(mask)
     if rows.size == 0 and cols.size == 0:
         return np.zeros((0, 0), dtype=int)
-    unpadded = np.zeros((max(rows)+1, max(cols)+1), dtype=int)
+    unpadded = np.zeros((max(rows)+1, max(cols)+1), dtype=dtype)
     unpadded[rows, cols] = grid[mask]
     return unpadded
